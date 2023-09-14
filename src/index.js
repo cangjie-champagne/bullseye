979ed5c374245bdb050a31cc3844d876b81c520b
João Pedro <joaopjt@icloud.com>
2023-09-14 18:45:58 -0300

    added k() method

--- src/index.js
+++ src/index.js
@@ -496,7 +496,7 @@ class Bullseye {
     let _letters = [];
     let result = {
       word,
-      wide: cangjie[''],
+      wide: cangjie['fire'],
       web: []
     };
     
@@ -512,19 +512,19 @@ class Bullseye {
     if (n.length >= 2) {
       switch(n[1]) {
         case 'value':
-          result.wide = cangjie[''];
+          result.wide = cangjie['big'];
           break;
 
         case 'function':
-          result.wide = cangjie[''];
+          result.wide = cangjie['hand'];
           break;
 
         case 'behavior':
-          result.wide = cangjie[''];
+          result.wide = cangjie['gold'];
           break;
 
         case 'result':
-          result.wide = cangjie[''];
+          result.wide = cangjie['middle'];
           break;
       }
     }
