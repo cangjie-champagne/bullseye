40cfe14d879483491854bd7514fd7084fc2ef9a6
João Pedro <joaopjt@icloud.com>
2023-09-14 17:38:06 -0300

    added a() method values

--- src/index.js
+++ src/index.js
@@ -100,7 +100,7 @@ class Bullseye {
     let _letters = [];
     let result = {
       word,
-      wide: cangjie[''],
+      wide: cangjie['twenty'],
       web: []
     };
     
@@ -116,19 +116,19 @@ class Bullseye {
     if (n.length >= 2) {
       switch(n[1]) {
         case 'value':
-          result.wide = cangjie[''];
+          result.wide = cangjie['disaster'];
           break;
 
         case 'function':
-          result.wide = cangjie[''];
+          result.wide = cangjie['hand'];
           break;
 
         case 'behavior':
-          result.wide = cangjie[''];
+          result.wide = cangjie['corpse'];
           break;
 
         case 'result':
-          result.wide = cangjie[''];
+          result.wide = cangjie['dagger-axe'];
           break;
       }
     }
